60a1011e78e1f9033d8855a9689d489090655c0b
Clint Modien <cmodien@gmail.com>
2017-11-12 22:19:08 -0600

    Fixing backup script

--- backup_preferences.py
+++ backup_preferences.py
@@ -7,6 +7,6 @@ domains = domains.split("\n")[0].split(", ")
 domains = ["NSGlobalDomain"] + domains
 
 for domain in domains:
-	filepath = BACKUP_DIR + domain + ".plist");
+	filepath = BACKUP_DIR + domain + ".plist";
 	print "Backing up: " + domain + " to " + filepath
 	executeShell(["defaults", "export", domain, filepath])
