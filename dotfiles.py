645b2461a916ea674ee22aaeff558fb77b004f17
Clint Modien <cmodien@gmail.com>
2017-11-23 10:11:41 -0600

    Fixing dotfiles unit tests. They don't hit the disk anymore

--- dotfiles.py
+++ dotfiles.py
@@ -1,34 +1,41 @@
 from os import path, listdir
-from config import get_dotfiles_backup_dir, get_dotfile_excludes, get_home_dir
-from utils import execute_shell
+from config import get_dotfiles_backup_dir, get_dotfile_excludes, get_home_dir, get_user
+from utils import execute_shell, ensure_files_owned_by_user
+
 
 def backup():
     print ''
     print 'Backuping up dotfiles...'
-    #build file list
+    # build file list
     home_dir = get_home_dir()
-    files = []
     excludes = get_dotfile_excludes()
-    for f in listdir(home_dir):
-        full_file_path = path.join(home_dir, f)
-        if path.isfile(full_file_path) and f[0] == '.' and f not in excludes:
-            files.append(full_file_path)
-    files.sort()
+    files = get_dot_files(home_dir, excludes)
     dest = get_dotfiles_backup_dir()
     command = ['cp', '-a', '-v'] + files + [dest]
     output = execute_shell(command)
     if output is not None:
         print output
 
+
 def restore():
     print ''
     print 'Restoring dotfiles...'
     source = get_dotfiles_backup_dir()
     dest = get_home_dir()
-    files = []
-    for f in listdir(source):
-        files.append(path.join(source, f))
-    command = ['cp', '-a', '-v'] + files + [dest]
+    files = get_dot_files(source)
+    command = ['sudo', 'cp', '-a', '-v'] + files + [dest]
     output = execute_shell(command)
     if output is not None:
         print output
+    ensure_files_owned_by_user(get_user(), files)
+
+
+def get_dot_files(home_dir, excludes=None):
+    if excludes is None:
+        excludes = []
+    files = []
+    for f in listdir(home_dir):
+        full_file_path = path.join(home_dir, f)
+        if path.isfile(full_file_path) and f[0] == '.' and f not in excludes:
+            files.append(full_file_path)
+    return files
--- test_dotfiles.py
+++ test_dotfiles.py
@@ -1,27 +1,41 @@
+from os import path
 import dotfiles
 from mock import patch
-from config import get_dotfiles_backup_dir, get_home_dir
+from config import get_dotfiles_backup_dir, get_home_dir, get_user
 
-
-@patch('utils.check_output')
-def test_backup(check_output_mock):
+@patch('dotfiles.get_dot_files')
+@patch('dotfiles.execute_shell')
+def test_backup(execute_shell_mock, dotfiles_mock):
+    files = ['.no_file']
+    dotfiles_mock.return_value = files
     dotfiles.backup()
-    # pylint: disable=unused-variable
-    args, kwargs = check_output_mock.call_args
-    command = args[0]
-    assert command[0] == 'cp'
-    assert command[1] == '-a'
-    assert command[-1] == get_dotfiles_backup_dir()
-    assert command[-1][-1] == '/'
+    dest = get_dotfiles_backup_dir()
+    execute_shell_mock.assert_called_with(
+        ['cp', '-a', '-v'] + files + [dest]
+    )
 
 
-@patch('utils.check_output')
-def test_restore(check_output_mock):
+@patch("dotfiles.ensure_files_owned_by_user")
+@patch('dotfiles.get_dot_files')
+@patch('dotfiles.execute_shell')
+def test_restore(execute_shell_mock, dotfiles_mock, ensure_mock):
+    files = ['.no_file']
+    dotfiles_mock.return_value = files
     dotfiles.restore()
-    # pylint: disable=unused-variable
-    args, kwargs = check_output_mock.call_args
-    command = args[0]
-    assert command[0] == 'cp'
-    assert command[1] == '-a'
-    assert command[-1] == get_home_dir()
-    assert command[-1][-1] == '/'
+    ensure_mock.assert_called_with(
+        get_user(), files
+    )
+    dest = get_home_dir()
+    execute_shell_mock.assert_called_with(
+        ['sudo', 'cp', '-a', '-v'] + files + [dest]
+    )
+
+@patch("dotfiles.path.isfile")
+@patch("dotfiles.listdir")
+def test_get_dot_files(listdir_mock, isfile_mock):
+    files = ['.testfile', '.no_file', '.good_file']
+    listdir_mock.return_value = files
+    isfile_mock.return_value = True
+    home_dir = get_home_dir()
+    result = dotfiles.get_dot_files(home_dir, ['.no_file'])
+    assert result[0] == path.join(home_dir, files[0])
