114c81028d314aad63d7e640e9632838fe4d8ba5
Sean Bugeja <seanbugeja23@gmail.com>
2023-07-28 17:28:48 +0200

    update formula template

--- macprefs.template.rb
+++ macprefs.template.rb
@@ -1,19 +1,18 @@
 class Macprefs < Formula
     include Language::Python::Virtualenv
-  
+
     desc "Backup and Restore your Mac System and App Preferences"
-    homepage "https://github.com/clintmod/macprefs"
-    url "https://github.com/clintmod/macprefs/archive/###version###.tar.gz"
+    homepage "https://github.com/sijanc147/macprefs"
+    url "https://github.com/sijanc147/macprefs/archive/###version###.tar.gz"
     sha256 "###sha256###"
-  
+
     depends_on :python3
-  
+
     def install
       bin.install Dir["*"]
     end
-  
+
     test do
       system "#{bin}/macprefs", "--help"
     end
   end
-  
