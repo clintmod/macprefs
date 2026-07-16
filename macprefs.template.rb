8865466b7a9a34702dce21d59d200141df76ce8f
Clint M <cmodien@gmail.com>
2026-07-16 09:53:19 -0600

    modernize brew formula (python@3.13, libexec install for clean uninstall #8), add GitHub Actions CI, update pylintrc for pylint 4

--- macprefs.template.rb
+++ macprefs.template.rb
@@ -1,19 +1,17 @@
 class Macprefs < Formula
-    include Language::Python::Virtualenv
-  
-    desc "Backup and Restore your Mac System and App Preferences"
-    homepage "https://github.com/clintmod/macprefs"
-    url "https://github.com/clintmod/macprefs/archive/###version###.tar.gz"
-    sha256 "###sha256###"
-  
-    depends_on :python3
-  
-    def install
-      bin.install Dir["*"]
-    end
-  
-    test do
-      system "#{bin}/macprefs", "--help"
-    end
+  desc "Backup and Restore your Mac System and App Preferences"
+  homepage "https://github.com/clintmod/macprefs"
+  url "https://github.com/clintmod/macprefs/archive/###version###.tar.gz"
+  sha256 "###sha256###"
+
+  depends_on "python@3.13"
+
+  def install
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"macprefs"
   end
-  
+
+  test do
+    system "#{bin}/macprefs", "--help"
+  end
+end
